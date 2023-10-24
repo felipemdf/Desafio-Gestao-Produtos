80ab94e1e547a206d1db301006003d0cec7fbd4c
felipe <f.marinho162001@gmail.com>
2023-10-24 14:11:12 -0300

    feat: criado a rota findAll

---
+++ src/resources/produtos/entities/dto/produto.dto.ts
@@ -0,0 +1,25 @@
+import { Produto } from '../produto.entity';
+
+export class ProdutoDto {
+  id: number;
+  descricao: string;
+  custo?: number;
+  imagem?: Buffer;
+
+  static ProdutoToProdutoDto(produtos: Produto[]): ProdutoDto[] {
+    const produtosDto: ProdutoDto[] = [];
+
+    for (const produto of produtos) {
+      const produtoDto: ProdutoDto = new ProdutoDto();
+
+      produtoDto.id = produto.id;
+      produtoDto.descricao = produto.descricao;
+      produtoDto.custo = produto.custo;
+      produtoDto.imagem = produto.imagem;
+
+      produtosDto.push(produtoDto);
+    }
+
+    return produtosDto;
+  }
+}
--- src/resources/produtos/produtos.controller.ts
+++ src/resources/produtos/produtos.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ProdutosService } from './produtos.service';
+import { ProdutoDto } from './entities/dto/produto.dto';
+import { FilterProdutoDto } from './entities/dto/filter-produto.dto';
 
 @Controller('produtos')
-export class ProdutosController {}
+export class ProdutosController {
+  constructor(private readonly produtoService: ProdutosService) {}
+
+  @Get()
+  async findAll(@Query() filtro: FilterProdutoDto): Promise<ProdutoDto[]> {
+    return await this.produtoService.findAll(filtro);
+  }
+}
